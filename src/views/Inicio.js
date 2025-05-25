d5efaa8b9dbd47d259b366911eedee6e3452d70e
ritta <ritavatteroni55555@gmail.com>
2025-05-25 01:09:02 -0300

    primeira tela feita (inicio)

--- src/views/Inicio.js
+++ src/views/Inicio.js
@@ -1,11 +1,97 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'; 
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Inicio({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>INICIO</Text>
-      <Button title="Login" onPress={() => navigation.navigate('Login')} />
-      <Button title="Cadastre-se" onPress={() => navigation.navigate('Cadastrar')} />
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#3E5698', '#141C32']}
+        style={styles.topSection}
+      >
+        <Image
+          source={require('../../assets/logo.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.WLCText}>Bem vindo ao Compass</Text>
+      </LinearGradient>  
+
+      <View>
+        <Text style={styles.description}>
+          Tenha o seu controle financeiro {'\n'} na palma das suas mãos
+        </Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity 
+        style={styles.loginButton}
+        onPress={() => navigation.navigate('Login')}
+        >
+          <Text style={styles.logintext}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.signupButton}
+          onPress={() => navigation.navigate('Cadastrar')}
+        >
+          <Text style={styles.signupText}>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#fff' },
+  topSection: {
+  flex: 0.9,
+  backgroundColor: '#3E5698, #141C32',
+  justifyContent: 'center',
+  borderBottomLeftRadius: 15,
+  borderBottomRightRadius: 15,
+  alignItems: 'center',
+},
+  logo: { width: 150, height: 150, marginBottom: 20 },
+  WLCText: {
+  color: '#fff',
+  fontSize: 25,
+  fontWeight: 'bold',
+},
+  description: {
+  marginBottom: -60,
+  marginTop: 20,
+  fontSize: 30,
+  fontWeight: 'bold',
+  color: '#000',
+  textAlign: 'center',
+},
+buttonContainer: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  paddingTop: 100,
+},
+  loginButton: {
+  backgroundColor: '#F5F5F5',
+  borderRadius: 20,
+  paddingVertical: 30,
+  paddingHorizontal: 70,   
+},
+  signupButton: {
+  backgroundColor: '#4153a3',
+  borderRadius: 20,
+  paddingVertical: 30,
+  paddingHorizontal: 50,
+},
+  signupText: {
+  color: '#fff',
+  fontSize: 20,
+  fontWeight: 'bold',
+},
+  logintext: {
+  color: '#000',
+  fontSize: 20,
+  fontWeight: 'bold',
+},
+
+
+});
