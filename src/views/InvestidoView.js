e4605fad4bb3f7e38265f2be4e9a9269a322c1cf
Emmanuel-J-dev <emmanueljunior.tec@gmail.com>
2025-05-29 11:51:27 -0300

    Telas Extra, Receber, investir, poupança e saude

--- src/views/InvestidoView.js
+++ src/views/InvestidoView.js
@@ -1,9 +1,112 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
-export default function InvestidoView() {
+export default function CustomPaymentScreen() {
+  const [amount, setAmount] = useState('');
+
+  const handleSave = () => {
+    alert(`Valor salvo: R$ ${amount}`);
+  };
+
   return (
-    <View>
-      <Text>Tela Investido</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Organize seus investimentos</Text>
+
+      <View style={styles.card}>
+        <Text style={styles.subtitle}>Renda Mensal</Text>
+        <View style={styles.incomeBox}>
+          <Text style={styles.income}>R$ 0,00</Text>
+        </View>
+      </View>
+
+      <View style={styles.paymentBox}>
+        <Text style={styles.paymentTitle}>insvestir</Text>
+        <Text style={styles.paymentLabel}>Insira o valor:</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="R$ 0,00"
+          value={amount}
+          onChangeText={setAmount}
+        />
+        <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+          <Text style={styles.saveText}>Salvar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: '#3A53A4',
+    borderRadius: 12,
+    padding: 20,
+    marginBottom: 30,
+  },
+  subtitle: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  incomeBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  income: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  paymentBox: {
+    backgroundColor: '#F7F7F7',
+    borderRadius: 20,
+    padding: 25,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  paymentTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  paymentLabel: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginBottom: 15,
+  },
+  input: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#4CAF50',
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#4CAF50',
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  saveButton: {
+    backgroundColor: '#F7C744',
+    paddingVertical: 12,
+    borderRadius: 25,
+    alignItems: 'center',
+  },
+  saveText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
